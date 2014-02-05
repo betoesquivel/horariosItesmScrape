7ef57701267b7256f77c8f2ac11aab016a63aa6d
Beto Esquivel <beto.esquivel.p@gmail.com>
2014-02-05 00:47:23 -0600

    Managed to scrape all emails with my schedule and print just the important information in my last file codeTagsContent5.txt

--- test.js
+++ test.js
@@ -1,4 +1,21 @@
 var MailListener = require("mail-listener2");
+var cheerio = require('cheerio'); 
+
+var identifiers = new Array(
+	"Profesor",
+	"Profesores",
+	"",
+	"CRQ",
+	"SPF",
+	"Curso Ciudadanía Transversal",
+	"Atributo",
+	"Planta",
+	"Fechas",
+	"DLMMJVS",
+	"Horario",
+	"Edificio",
+	"Salón"
+);
 
 var mailListener = new MailListener({
   username: "horarios.monterrey@gmail.com",
@@ -32,7 +49,33 @@ mailListener.on("error", function(err){
 mailListener.on("mail", function(mail){
   // do something with mail object including attachments
   var subject = mail.subject;
-  if(subject.indexOf("Tu horario de clases del periodo")!=-1)
-	  console.log("emailParsed", mail.html);
+  if(subject.indexOf("Tu horario de clases del periodo")!=-1){
+	  console.log("======emailParsed======");
+	  $ = cheerio.load(mail.html);
+
+	  //print all <code></code> html tags content in email
+	  $( "code" ).each(function() {
+		var text = $(this).text().trim();
+		if ( !(identifiers.indexOf(text) > -1) ) {
+			console.log(text);
+			//if it is a number, then its the last field
+			if( !isNaN(text) ){
+				console.log(""); 	
+			}
+		}
+	  });
+  }
   // mail processing code goes here
 });
+
+
+// /*
+//  *
+//  * Notas: 
+//  * En el html estas tags tienen este tipo de contenido
+//  * <code> Materia con todo y grupo </code>
+//  tambien tiene profesor, fechas, horarios, salon
+//  todo viene entre code
+//  *
+//  *
+//  * /
